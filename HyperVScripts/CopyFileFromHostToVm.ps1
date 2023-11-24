8ed8abc8433c3c8350cb009f59c5250b3b6ded17
DALGA Alexandre <alexandre.dalga@spie.com>
2023-11-24 15:45:07 +0100

    Ajout de mon dossier de travail

---
+++ HyperVScripts/CopyFileFromHostToVm.ps1
@@ -0,0 +1,11 @@
+﻿Get-VM | ft
+$VmName = (Get-VM).name
+$SourceFile = "C:\ProfWorkFolder\Intune\Get-InstalledSoftware.ps1"
+$DestinationPath = "C:\Data\Get-InstalledSoftware.ps1"
+
+$VmName = "vmWin11Hachette"
+$SourceFile = Read-host "Enter source file path"
+$FileName = Read-Host "Enter the source file name"
+$DestinationPath = "C:\Data\$FileName"
+
+Copy-VMFile $VmName -SourcePath $SourceFile -DestinationPath $DestinationPath -CreateFullPath -FileSource Host
